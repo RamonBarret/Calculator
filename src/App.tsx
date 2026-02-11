6aababb0b0d429a1bc1785f13607d408896c88ab
gustavo-shigueo <58121396+gustavo-shigueo@users.noreply.github.com>
2026-02-11 18:12:33 -0300

    Test new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import "./App.css";
+import { Canvas } from "./components/layout/Canvas";
 import { Button, ButtonVariant } from "./components/common/Button";
 import { Display } from "./components/common/display/Display";
 import { ThemeSwitch } from "./components/common/ThemeSwitch";
+import {
+  StackPanel,
+  StackPanelAlignment,
+  StackPanelDirection,
+} from "./components/layout/StackPanel";
 
 function App() {
   return (
-    <>
-      <ThemeSwitch />
-      <Button variant={ButtonVariant.Primary}>test</Button>
-      <br />
-      <br />
-      <Button variant={ButtonVariant.Secondary}>test</Button>
-      <br />
-      <br />
-      <Button variant={ButtonVariant.Accent}>test</Button>
-      <br />
-      <br />
-      <Display value={"399,981"} />
-    </>
+    <StackPanel
+      orientation={StackPanelDirection.Vertical}
+      alignItems={StackPanelAlignment.Center}
+      justifyContent={StackPanelAlignment.Center}
+    >
+      <Canvas width="min(540px, 100% - 48px)">
+        <ThemeSwitch />
+        <Button variant={ButtonVariant.Primary}>test</Button>
+        <br />
+        <br />
+        <Button variant={ButtonVariant.Secondary}>test</Button>
+        <br />
+        <br />
+        <Button variant={ButtonVariant.Accent}>test</Button>
+        <br />
+        <br />
+        <Display value={"399,981"} />
+      </Canvas>
+    </StackPanel>
   );
 }
 
