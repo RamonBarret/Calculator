748f283c69d903a62bbfebab1c8b2747d2278dc4
gustavo-shigueo <58121396+gustavo-shigueo@users.noreply.github.com>
2026-02-11 18:12:26 -0300

    Create canvas

---
+++ src/components/layout/Canvas/Canvas.tsx
@@ -0,0 +1,10 @@
+import type { FC, ReactNode } from "react";
+
+interface CanvasProps {
+  width?: string | number;
+  children?: ReactNode;
+}
+
+export const Canvas: FC<CanvasProps> = ({ width, children }) => {
+  return <div style={{ width, background: "pink" }}>{children}</div>;
+};
